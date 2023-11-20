4868597395f5097a45cb4008f945d964bd2c9238
yelo-o <flyordig@gmail.com>
2023-11-20 16:57:20 +0900

    pokemon service update

--- src/main/java/com/pokemonreview/api/service/impl/PokemonServiceImpl.java
+++ src/main/java/com/pokemonreview/api/service/impl/PokemonServiceImpl.java
@@ -6,6 +6,7 @@ import com.pokemonreview.api.exceptions.ResourceNotFoundException;
 import com.pokemonreview.api.models.Pokemon;
 import com.pokemonreview.api.repository.PokemonRepository;
 import com.pokemonreview.api.service.PokemonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,24 +19,24 @@ import java.util.stream.Collectors;
 
 @Service
 @Transactional
+@RequiredArgsConstructor //final로 선언된 변수 생성자 초기화
 public class PokemonServiceImpl implements PokemonService {
-    private PokemonRepository pokemonRepository;
+    private final PokemonRepository pokemonRepository;
 
-    public PokemonServiceImpl(PokemonRepository pokemonRepository) {
-        this.pokemonRepository = pokemonRepository;
-    }
+    //생성자 주입
+//    public PokemonServiceImpl(PokemonRepository pokemonRepository) {
+//        this.pokemonRepository = pokemonRepository;
+//    }
 
     @Override
     public PokemonDto createPokemon(PokemonDto pokemonDto) {
         Pokemon pokemon = mapToEntity(pokemonDto);
-
         Pokemon newPokemon = pokemonRepository.save(pokemon);
-
-        PokemonDto pokemonResponse = new PokemonDto();
-        pokemonResponse.setId(newPokemon.getId());
-        pokemonResponse.setName(newPokemon.getName());
-        pokemonResponse.setType(newPokemon.getType());
-        return pokemonResponse;
+//        PokemonDto pokemonResponse = new PokemonDto();
+//        pokemonResponse.setId(newPokemon.getId());
+//        pokemonResponse.setName(newPokemon.getName());
+//        pokemonResponse.setType(newPokemon.getType());
+        return mapToDto(newPokemon);
     }
 
     @Override
@@ -47,16 +48,17 @@ public class PokemonServiceImpl implements PokemonService {
         List<Pokemon> listOfPokemon = pokemonPage.getContent();
         List<PokemonDto> content = listOfPokemon
                 .stream() //Stream<Pokemon>
-                .map(p -> mapToDto(p)) //Stream<PokemonDto>
-                //.map(this::mapToDto)
+                //entity -> dto
+                //.map(entity -> mapToDto(entity)) //Stream<PokemonDto>
+                .map(this::mapToDto)
                 .collect(Collectors.toList()); //List<PokemonDto>
 
         PageResponse<PokemonDto> pokemonResponse = new PageResponse<>();
         pokemonResponse.setContent(content);
-        pokemonResponse.setPageNo(pokemonPage.getNumber());
-        pokemonResponse.setPageSize(pokemonPage.getSize());
-        pokemonResponse.setTotalElements(pokemonPage.getTotalElements());
-        pokemonResponse.setTotalPages(pokemonPage.getTotalPages());
+        pokemonResponse.setPageNo(pokemonPage.getNumber()); //페이지 번호
+        pokemonResponse.setPageSize(pokemonPage.getSize()); //페이지 사이즈
+        pokemonResponse.setTotalElements(pokemonPage.getTotalElements()); //전체 엘리멘트 개수
+        pokemonResponse.setTotalPages(pokemonPage.getTotalPages()); //전체 엘리멘트 개수/페이지 사이즈
         pokemonResponse.setLast(pokemonPage.isLast());
 
         return pokemonResponse;
@@ -69,21 +71,21 @@ public class PokemonServiceImpl implements PokemonService {
     }
 
     private Pokemon getExistPokemon(int id) {
-        Pokemon pokemon = pokemonRepository
-                .findById(id)
+        return pokemonRepository
+                .findById(id) //Optional<Pokemon>
                 .orElseThrow(() ->
                         new ResourceNotFoundException("Pokemon could not be found"));
-        return pokemon;
     }
 
     @Override
     public PokemonDto updatePokemon(PokemonDto pokemonDto, int id) {
         Pokemon pokemon = getExistPokemon(id);
 
+        //Entity의 setter method 호출을 해도 update query가 실행 됨(Dirty checking)
         pokemon.setName(pokemonDto.getName());
         pokemon.setType(pokemonDto.getType());
 
-        //Pokemon updatedPokemon = pokemonRepository.save(pokemon);
+        //Pokemon updatedPokemon = pokemonRepository.save(pokemon); -> Transactional이 걸려있기 때문에 할 필요 없음
         return mapToDto(pokemon);
     }
 
@@ -93,6 +95,7 @@ public class PokemonServiceImpl implements PokemonService {
         pokemonRepository.delete(pokemon);
     }
 
+    //Entity -> Dto
     private PokemonDto mapToDto(Pokemon pokemon) {
         PokemonDto pokemonDto = new PokemonDto();
         pokemonDto.setId(pokemon.getId());
@@ -101,6 +104,7 @@ public class PokemonServiceImpl implements PokemonService {
         return pokemonDto;
     }
 
+    //Dto -> Entity
     private Pokemon mapToEntity(PokemonDto pokemonDto) {
         Pokemon pokemon = new Pokemon();
         pokemon.setName(pokemonDto.getName());

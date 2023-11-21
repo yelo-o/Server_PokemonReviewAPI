1cf58d527121df9944db467e2d50062905b17693
yelo-o <flyordig@gmail.com>
2023-11-21 10:26:46 +0900

    add dto of security

---
+++ src/main/java/com/pokemonreview/api/dto/security/UserDto.java
@@ -0,0 +1,26 @@
+package com.pokemonreview.api.dto.security;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class UserDto {
+    private int id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String role;
+
+    @Builder
+    public UserDto(int id, String username, String firstName, 
+String lastName, String role) {
+        this.id = id;
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+}

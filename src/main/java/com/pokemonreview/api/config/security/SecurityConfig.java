2b5f1e9de647bd2efeffa23c8d91cfb924e8059d
yelo-o <flyordig@gmail.com>
2023-11-21 13:50:34 +0900

    add MethodSecurity in SecurityConfig

--- src/main/java/com/pokemonreview/api/config/security/SecurityConfig.java
+++ src/main/java/com/pokemonreview/api/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,6 +24,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     private CustomUserDetailsService userDetailsService;
 

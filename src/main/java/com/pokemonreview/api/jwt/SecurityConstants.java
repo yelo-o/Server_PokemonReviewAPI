2e41d98a9434c79b6d1e7522b34e7f3e09d88565
yelo-o <flyordig@gmail.com>
2023-11-21 11:21:35 +0900

    add DetailService

--- src/main/java/com/pokemonreview/api/jwt/SecurityConstants.java
+++ src/main/java/com/pokemonreview/api/jwt/SecurityConstants.java
@@ -1,5 +1,5 @@
 package com.pokemonreview.api.jwt;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 86400000;
+    public static final long JWT_EXPIRATION = 86400000; //24h=8640000s
 }

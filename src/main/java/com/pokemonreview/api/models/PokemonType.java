d4b60c66585b2bfea4a853f18fd9c1c511074221
yelo-o <flyordig@gmail.com>
2023-11-21 09:21:09 +0900

    Update PokemnonType for application of enum type json

--- src/main/java/com/pokemonreview/api/models/PokemonType.java
+++ src/main/java/com/pokemonreview/api/models/PokemonType.java
@@ -1,18 +1,33 @@
 package com.pokemonreview.api.models;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public enum PokemonType {
-    NORMAL, FIRE, WATER, ELECTRIC, GRASS,
-    ICE, FIGHTING, POISON, GROUND, FLYING,
-    PSYCHIC, BUG, ROCK, GHOST, DRAGON,
-    DARK, STEEL, FAIRY;
+    NORMAL("NORMAL"), FIRE("FIRE"), WATER("WATER"),
+    ELECTRIC("ELECTRIC"), GRASS("GRASS"), ICE("ICE"),
+    FIGHTING("FIGHTING"), POISON(""), GROUND("GROUND"),
+    FLYING("FLYING"), PSYCHIC("PSYCHIC"), BUG("BUG"),
+    ROCK("ROCK"), GHOST("GHOST"), DRAGON("DRAGON"),
+    DARK("DARK"), STEEL("STEEL"), FAIRY("FAIRY");
+
+    private final String name;
+
+    PokemonType(String name) {
+        this.name = name;
+    }
+
+    @JsonValue
+    public String getName() {
+        return this.name;
+    }
 
     public static List<String> nameList() {
         return Arrays.stream(PokemonType.values())
-                .map(value -> value.name())
-                .collect(Collectors.toList());
+                //.map(value -> value.name())
+                .map(Enum::name)
+                .collect(Collectors.toList());  //List<String>
     }
 }

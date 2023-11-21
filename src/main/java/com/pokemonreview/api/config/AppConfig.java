e9181f200725d6fdcc7fde3a87676b5d8fefd838
yelo-o <flyordig@gmail.com>
2023-11-21 09:31:04 +0900

    Add review insert querys in AppConfig

--- src/main/java/com/pokemonreview/api/config/AppConfig.java
+++ src/main/java/com/pokemonreview/api/config/AppConfig.java
@@ -2,7 +2,9 @@ package com.pokemonreview.api.config;
 
 import com.pokemonreview.api.models.Pokemon;
 import com.pokemonreview.api.models.PokemonType;
+import com.pokemonreview.api.models.Review;
 import com.pokemonreview.api.repository.PokemonRepository;
+import com.pokemonreview.api.repository.ReviewRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +18,7 @@ import java.util.stream.IntStream;
 @Profile("test")
 public class AppConfig {
     @Bean
-    public CommandLineRunner test(PokemonRepository pokemonRepository) {
+    public CommandLineRunner test(PokemonRepository pokemonRepository, ReviewRepository reviewRepository) {
         return args -> {
             System.out.println("**** Pokemon Insert 시작");
 //            pokemonRepository.deleteAll(); //ddl-auto:create면 주석처리해도 됨
@@ -29,6 +31,28 @@ public class AppConfig {
 
             pokemonRepository.saveAll(pokemonList);
             System.out.println("**** Pokemon Insert 끝");
+
+            System.out.println("#### Review Insert 시작");
+            pokemonRepository.findAll() //List<Pokemon>
+                    .forEach(pokemon -> {
+                        Review review = Review.builder()
+                                .title(pokemon.getName() + " title")
+                                .content(pokemon.getName() + " content")
+                                .stars(5)
+                                .pokemon(pokemon)
+                                .build();
+                        reviewRepository.save(review);
+
+                        Review review2 = Review.builder()
+                                .title(pokemon.getType().getName() + " title")
+                                .content(pokemon.getType().getName() + " content")
+                                .stars(9)
+                                .pokemon(pokemon)
+                                .build();
+                        reviewRepository.save(review2);
+
+                    });
+            System.out.println("#### Review Insert 끝");
 
         };
     }
